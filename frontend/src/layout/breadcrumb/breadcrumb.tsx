7459a09fe06a77c8a7bac7df955b9fc2467b34a4
Toan Le <toan.le@vtl-vtl.com>
2021-07-08 14:48:06 +0700

    update react TS

--- frontend/src/layout/breadcrumb/breadcrumb.tsx
+++ frontend/src/layout/breadcrumb/breadcrumb.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import { Breadcrumb } from 'antd';
-import { useRouteMatch } from 'react-router-dom';
 import './breadcrumb.scss';
 
-const IBreadcrumb: React.FC = () => {
-  const match = useRouteMatch();
-  console.log(match);
-
-  return (
-    <Breadcrumb style={{ margin: '10px 0' }}>
-      <Breadcrumb.Item href="/">Home </Breadcrumb.Item>
-      <Breadcrumb.Item>List</Breadcrumb.Item>
-      <Breadcrumb.Item>App</Breadcrumb.Item>
-      <Breadcrumb.Item>App</Breadcrumb.Item>
-    </Breadcrumb>
-  );
-};
+const IBreadcrumb: React.FC = () => (
+  <Breadcrumb style={{ margin: '10px 0' }}>
+    <Breadcrumb.Item href="/">Home </Breadcrumb.Item>
+    <Breadcrumb.Item>List</Breadcrumb.Item>
+    <Breadcrumb.Item>App</Breadcrumb.Item>
+    <Breadcrumb.Item>App</Breadcrumb.Item>
+  </Breadcrumb>
+);
 
 export default IBreadcrumb;
